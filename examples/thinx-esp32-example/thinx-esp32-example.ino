6819c3f1897ea45df5821c77bdca9b0c49ab07c7
Matěj Sychra <suculent@me.com>
2018-03-19 15:03:37 +0100

    synchronized implementation with all latest features from THiNXLib for ESP8266 version 2.1.170 (time support, firmware update, auto-reboot, auto-checkin, MQTT callbacks, app version, public udid and many other fixes)

--- examples/thinx-esp32-example/thinx-esp32-example.ino
+++ examples/thinx-esp32-example/thinx-esp32-example.ino
@@ -1,25 +1,12 @@
 #include <Arduino.h>
+
+// 1. Include
 #include <THiNX32Lib.h>
 
 #define ARDUINO_IDE
+#define __DEBUG__ // wait for serial port on boot
 
-
-// Import SSID, PASSWORD and API KEY from file not included in repository
-#ifndef THINXCI
-#import "settings.h"
-#else
-// Those are just dummy demo values. Never store those in public repository!
-// Save them to settings.h and use .gitigore to keep file on your machine only.
-// THiNX CI will be able to add the apikey and owner_id on its own. You can configure
-// optinally THINX_ENV_SSID and THINX_ENV_PASS Environment Variables to inject those from CI.
-
-// Add this to your settings.h and ignore this file from repository before committing!
-const char *apikey = "";
-const char *owner_id = "";
-const char *ssid = "THiNX-IoT+";
-const char *pass = "<enter-your-ssid-password>";
-#endif
-
+// 2. Declare
 THiNX thx;
 
 /* Called after library gets connected and registered */
@@ -34,18 +21,18 @@ void setup() {
 
 #ifdef __DEBUG__
   while (!Serial); // wait for debug console connection
-  WiFi.begin(ssid, pass);
+  WiFi.begin("THiNX-IoT", "<enter-your-ssid-password>");
 #endif
 
-  thx = THiNX(apikey);
-
-  // Enter API Key and Owner ID (should be faster)
-  //thx = THiNX(apikey, owner_id);
+  // 3. initialize with API Key and Owner ID
+  thx = THiNX("71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732", "cedc16bb6bb06daaa3ff6d30666d91aacd6e3efbf9abbc151b4dcade59af7c12"); 
+  
   thx.setFinalizeCallback(finalizeCallback);
 }
 
 /* Loop must call the thx.loop() in order to pickup MQTT messages and advance the state machine. */
 void loop()
 {
+  // 4. Runloop: waits for WiFI, registers, checks MQTT, reconnects, updates, processes changes...
   thx.loop();
 }
